c9adac2353eb495f36572885a288c1ed949e18db
ThatBritishKid <hexuscharlie@gmail.com>
2017-07-08 22:43:05 +0100

    Fixed Incorrect Params

--- asyncIDEmbed.js
+++ asyncIDEmbed.js
@@ -1,11 +1,11 @@
 //Send an embed to a specific channel via provided parameter.
-async function embed(id, c, t, d) {
+async function embed(id, colour, title, description) {
   try {
     msg.guild.channels.get(id).send("", {embed: {
       author: {name: msg.author.username, iconURL: msg.author.displayAvatarURL},
-      color: c,
-      title: t,
-      description: c,
+      color: colour,
+      title: title,
+      description: description,
       timestamp: new Date(),
     }});
   } catch(e) {
