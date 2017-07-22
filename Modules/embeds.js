b868de58990e9dbeea3bfab26e2b5ce62399d043
Hexus Charlie <hexuscharlie@gmail.com>
2017-07-22 14:50:51 +0100

    added global embed, and comments

--- Modules/embeds.js
+++ Modules/embeds.js
@@ -1,4 +1,5 @@
 module.exports = {
+  //Normal current channel embed
   embed: async function(msg, colour, title, description) {
     try {
       await msg.channel.send("", {embed: {
@@ -12,6 +13,7 @@ module.exports = {
     console.error(e);
     }
   },
+  //Sends embed to specific channel
   embedID: async function(msg, id, colour, title, description) {
     try {
       await msg.guild.channels.get(id).send("", {embed: {
@@ -25,10 +27,25 @@ module.exports = {
       console.error(e);
     }
   },
+  //Send embed to members dm's
   embedDM: async function(msg, userid, colour, title, description) {
     try {
       await msg.guild.members.get(userid).send("", {embed: {
-        author: {name: msg.tag, iconURL: msg.author.displayAvatarURL},
+        author: {name: msg.author.tag, iconURL: msg.author.displayAvatarURL},
+        color: colour,
+        title: title,
+        description: description,
+        timestamp: new Date(),
+      }});
+    } catch(e) {
+      console.error(e);
+    }
+  },
+  //Sends embed to any cached channel in your bot
+  embedGID: async function(bot, msg, channelid, colour, title, description) {
+    try {
+      await bot.channels.get(channelid).send("", {embed: {
+        author: {name: msg.author.tag, iconURL: msg.author.displayAvatarURL},
         color: colour,
         title: title,
         description: description,
