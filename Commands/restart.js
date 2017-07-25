227410770306bfbc0018d1db87ededfc6b195b2d
Hexus Charlie <hexuscharlie@gmail.com>
2017-07-25 22:22:53 +0100

    Removed silly embed function

--- Commands/restart.js
+++ Commands/restart.js
@@ -1,21 +1,5 @@
-exports.run = async function(bot, msg, args) {
-  async function embed(colour, title, description) {
-    try {
-    await msg.channel.send("", {embed: {
-      author: {
-        name: msg.author.username,
-        icon_url: msg.author.avatarURL,
-      },
-      color: colour,
-      title: title,
-      description: description,
-      timestamp: new Date(),
-    }});
-    } catch(e) {
-    console.error(e);
-    }
-  };
-  embed(0xff0000, "Restarting main bot file!", "Now processing...").then(() => {process.exit(1)});
+exports.run = async function(bot, msg) {
+  msg.reply("Now restarting my bot file!").then(() => {process.exit(1)});
 };
 
 exports.conf = {
