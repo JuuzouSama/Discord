b4e9c086738e376cf5eac8ab538751bf84a96652
Charlie <hexuscharlie@gmail.com>
2017-09-12 18:40:43 +0100

    Add files via upload

---
+++ Bots/Selfbot/embed.js
@@ -0,0 +1,72 @@
+module.exports = {
+  //Normal current channel embed
+  embed: async function(msg, colour, title, description) {
+    try {
+      await msg.channel.send("", {embed: {
+        author: {name: msg.author.tag, iconURL: msg.author.displayAvatarURL},
+        color: colour,
+        title: title,
+        description: description,
+        timestamp: new Date(),
+      }});
+    } catch(e) {
+    console.error(e);
+    }
+  },
+  //Sends embed to specific channel
+  embedID: async function(msg, id, colour, title, description) {
+    try {
+      await msg.guild.channels.get(id).send("", {embed: {
+        author: {name: msg.author.tag, iconURL: msg.author.displayAvatarURL},
+        color: colour,
+        title: title,
+        description: description,
+        timestamp: new Date(),
+      }});
+    } catch(e) {
+      console.error(e);
+    }
+  },
+  //Send embed to members dm's
+  embedDM: async function(msg, userid, colour, title, description) {
+    try {
+      await msg.guild.members.get(userid).send("", {embed: {
+        author: {name: msg.author.tag, iconURL: msg.author.displayAvatarURL},
+        color: colour,
+        title: title,
+        description: description,
+        timestamp: new Date(),
+      }});
+    } catch(e) {
+      console.error(e);
+    }
+  },
+  //Sends embed to any cached channel in your bot
+  embedGID: async function(bot, msg, channelid, colour, title, description) {
+    try {
+      await bot.channels.get(channelid).send("", {embed: {
+        author: {name: msg.author.tag, iconURL: msg.author.displayAvatarURL},
+        color: colour,
+        title: title,
+        description: description,
+        timestamp: new Date(),
+      }});
+    } catch(e) {
+      console.error(e);
+    }
+  },
+  //Edits your current message into an embed!
+  embedEdit: async function(msg, colour, title, description) {
+      try {
+        await msg.edit("", {embed: {
+        author: {name: msg.author.tag, iconURL: msg.author.avatarURL},
+        color: colour,
+        title: title,
+        description: description,
+        timestamp: new Date(),
+      }});
+    } catch(e) {
+      console.error(e);
+    }
+  }
+};
